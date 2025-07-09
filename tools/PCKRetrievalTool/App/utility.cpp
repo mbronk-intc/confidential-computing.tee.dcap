b0d2642f94392215b6fa8af6a13d5c7f44c9184f
Pawel Drzycimski <pawel.drzycimski@intel.com>
2025-07-09 13:00:14 +0000

    Extended buffer for bigger Platform Manifest size. Added support for version 3 of attestation header. Refactored directory structure for UT.
    Signed-off-by: Drzycimski Pawel <pawel.drzycimski@intel.com>

--- tools/PCKRetrievalTool/App/utility.cpp
+++ tools/PCKRetrievalTool/App/utility.cpp
@@ -115,7 +115,7 @@ typedef sgx_status_t (SGXAPI* sgx_get_target_info_func_t)(const sgx_enclave_id_t
 #include "MPUefi.h"
 typedef MpResult(*mp_uefi_init_func_t)(const char* path, const LogLevel logLevel);
 typedef MpResult(*mp_uefi_get_request_type_func_t)(MpRequestType* type);
-typedef MpResult(*mp_uefi_get_request_func_t)(uint8_t *request, uint16_t *request_size);
+typedef MpResult(*mp_uefi_get_request_func_t)(uint8_t *request, uint32_t *request_size);
 typedef MpResult(*mp_uefi_get_registration_status_func_t)(MpRegistrationStatus* status);
 typedef MpResult(*mp_uefi_set_registration_status_func_t)(const MpRegistrationStatus* status);
 typedef MpResult(*mp_uefi_terminate_func_t)();
@@ -293,7 +293,7 @@ void unload_enclave(sgx_enclave_id_t* p_eid)
 //  UEFI_OPERATION_LIB_NOT_AVAILABLE: it means that the uefi shared library doesn't exist
 //  UEFI_OPERATION_FAIL:  it is one add package request, now we don't support it. 
 //  UEFI_OPERATION_UNEXPECTED_ERROR: error happens.
-uefi_status_t get_platform_manifest(uint8_t ** buffer, uint16_t &out_buffer_size)
+uefi_status_t get_platform_manifest(uint8_t ** buffer, uint32_t &out_buffer_size)
 {
     uefi_status_t ret = UEFI_OPERATION_UNEXPECTED_ERROR;
 #ifdef _MSC_VER
@@ -323,7 +323,7 @@ uefi_status_t get_platform_manifest(uint8_t ** buffer, uint16_t &out_buffer_size
 #endif
     mp_uefi_init_func_t p_mp_uefi_init = (mp_uefi_init_func_t)FINDFUNCTIONSYM(uefi_lib_handle, "mp_uefi_init");
     mp_uefi_get_request_type_func_t p_mp_uefi_get_request_type = (mp_uefi_get_request_type_func_t)FINDFUNCTIONSYM(uefi_lib_handle, "mp_uefi_get_request_type");
-    mp_uefi_get_request_func_t p_mp_uefi_get_request = (mp_uefi_get_request_func_t)FINDFUNCTIONSYM(uefi_lib_handle, "mp_uefi_get_request");
+    mp_uefi_get_request_func_t p_mp_uefi_get_request = (mp_uefi_get_request_func_t)FINDFUNCTIONSYM(uefi_lib_handle, "mp_uefi_get_request_ex");
     mp_uefi_get_registration_status_func_t p_mp_uefi_get_registration_status = (mp_uefi_get_registration_status_func_t)FINDFUNCTIONSYM(uefi_lib_handle, "mp_uefi_get_registration_status");
     mp_uefi_terminate_func_t p_mp_uefi_terminate = (mp_uefi_terminate_func_t)FINDFUNCTIONSYM(uefi_lib_handle, "mp_uefi_terminate");
     if (p_mp_uefi_init == NULL ||
@@ -348,7 +348,7 @@ uefi_status_t get_platform_manifest(uint8_t ** buffer, uint16_t &out_buffer_size
         mpResult = p_mp_uefi_get_request_type(&type);
         if (mpResult == MP_SUCCESS) {
             if (type == MP_REQ_REGISTRATION) {
-                *buffer = new (std::nothrow) unsigned char[UINT16_MAX];
+                *buffer = new (std::nothrow) unsigned char[UINT32_MAX];
                 mpResult = p_mp_uefi_get_request(*buffer, &out_buffer_size);
                 if (mpResult != MP_SUCCESS) {
                     printf("Error: Couldn't get the platform manifest information.\n");
@@ -696,9 +696,9 @@ uint8_t convert_value_to_ascii(uint8_t in)
 //       out_buf, output the HEX encoding of in_buf on success.
 //@return true on success and false on error
 //The out_size must always be 2*in_size since each byte into encoded by 2 characters
-bool byte_array_to_hex_string(const uint8_t *in_buf, uint32_t in_size, uint8_t *out_buf, uint32_t out_size)
+bool byte_array_to_hex_string(const uint8_t *in_buf, uint64_t in_size, uint8_t *out_buf, uint64_t out_size)
 {
-	if (in_size>UINT32_MAX / 2)return false;
+	if (in_size>UINT64_MAX / 2)return false;
 	if (in_buf == NULL || out_buf == NULL || out_size != in_size * 2)return false;
 
 	for (uint32_t i = 0; i< in_size; i++)
@@ -720,7 +720,7 @@ bool byte_array_to_hex_string(const uint8_t *in_buf, uint32_t in_size, uint8_t *
 *
 * @return true If the byte array was appended to the UR successfully
 */
-network_post_error_t append_body_context(string& url, const uint8_t* request, const uint32_t request_size)
+network_post_error_t append_body_context(std::string& url, const uint8_t* request, const uint64_t request_size)
 {
 	if (request_size >= UINT32_MAX / 2)
 		return POST_INVALID_PARAMETER_ERROR;
@@ -738,10 +738,10 @@ network_post_error_t append_body_context(string& url, const uint8_t* request, co
 }
 
 network_post_error_t generate_json_message_body(const uint8_t *raw_data, 
-                                                const uint32_t raw_data_size,
+                                                const uint64_t raw_data_size,
                                                 const uint16_t platform_id_length,
                                                 const bool non_enclave_mode, 
-                                                string &jsonString)
+                                                std::string &jsonString)
 {
     network_post_error_t ret = POST_SUCCESS;
     const uint8_t *position = raw_data;
@@ -765,7 +765,7 @@ network_post_error_t generate_json_message_body(const uint8_t *raw_data,
         }
     }
     else {
-        uint32_t left_size = raw_data_size - platform_id_length - CPU_SVN_LENGTH - ISV_SVN_LENGTH - PCE_ID_LENGTH - ENCRYPTED_PPID_LENGTH;
+        uint64_t left_size = raw_data_size - platform_id_length - CPU_SVN_LENGTH - ISV_SVN_LENGTH - PCE_ID_LENGTH - ENCRYPTED_PPID_LENGTH;
         jsonString += "\"enc_ppid\": \"";
         if ((ret = append_body_context(jsonString, position, ENCRYPTED_PPID_LENGTH)) != POST_SUCCESS) {
             return ret;
